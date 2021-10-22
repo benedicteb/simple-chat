f7277e2f090f7cf39d7cce23da2c4fb9eeca86fc
Benedicte Emilie Brækken <b@brkn.io>
2021-10-22 10:37:59 +0200

    B Fix bug where chat components were switched

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,7 +1,7 @@
 const ChatMessage = ({ children, name, avatarSrc }) => {
   return (
-    <div className="chatlog-message chatlog-message--right chatlog-message--is-display-avatar chatlog-message--is-first-in-group chatlog-message--is-last-in-group">
-      <div aria-hidden="true" className="chatlog-avatar chatlog-avatar--right">
+    <div className="chatlog-message chatlog-message--left chatlog-message--is-display-avatar chatlog-message--is-first-in-group chatlog-message--is-last-in-group">
+      <div aria-hidden="true" className="chatlog-avatar chatlog-avatar--left">
         <img
           className="chatlog-avatarImage"
           title={`${name}`}
@@ -13,15 +13,15 @@ const ChatMessage = ({ children, name, avatarSrc }) => {
       </div>
       <div
         role="text"
-        className="chatlog-message__content chatlog-message__content--right chatlog-message__content--has-name"
+        className="chatlog-message__content chatlog-message__content--left chatlog-message__content--has-name"
       >
         <span
           aria-hidden="true"
-          className="chatlog-message__name chatlog-message__name--right"
+          className="chatlog-message__name chatlog-message__name--left"
         >
           {name}
         </span>
-        <span className="chatlog-message__text chatlog-message__text--right">
+        <span className="chatlog-message__text chatlog-message__text--left">
           {children}
         </span>
       </div>
--- src/components/ChatMessageReply.jsx
+++ src/components/ChatMessageReply.jsx
@@ -1,7 +1,7 @@
 const ChatMessageReply = ({ children, name, avatarSrc }) => {
   return (
-    <div className="chatlog-message chatlog-message--left chatlog-message--is-display-avatar chatlog-message--is-first-in-group chatlog-message--is-last-in-group">
-      <div aria-hidden="true" className="chatlog-avatar chatlog-avatar--left">
+    <div className="chatlog-message chatlog-message--right chatlog-message--is-display-avatar chatlog-message--is-first-in-group chatlog-message--is-last-in-group">
+      <div aria-hidden="true" className="chatlog-avatar chatlog-avatar--right">
         <img
           className="chatlog-avatarImage"
           title={`${name}`}
@@ -13,15 +13,15 @@ const ChatMessageReply = ({ children, name, avatarSrc }) => {
       </div>
       <div
         role="text"
-        className="chatlog-message__content chatlog-message__content--left chatlog-message__content--has-name"
+        className="chatlog-message__content chatlog-message__content--right chatlog-message__content--has-name"
       >
         <span
           aria-hidden="true"
-          className="chatlog-message__name chatlog-message__name--left"
+          className="chatlog-message__name chatlog-message__name--right"
         >
           {name}
         </span>
-        <span className="chatlog-message__text chatlog-message__text--left">
+        <span className="chatlog-message__text chatlog-message__text--right">
           {children}
         </span>
       </div>
