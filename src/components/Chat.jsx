8912ef6e6019d6a221559bdebabc6e564fd5407a
Benedicte Emilie Brækken <b@brkn.io>
2021-10-24 19:31:23 +0200

    Add styling for scrolling view

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,5 @@
 const Chat = ({ children }) => (
-  <div className="chatlog-root">
+  <div className="chatlog-root" id={"chatlog-root"}>
     <div className="chatlog-app">
       <div className="chatlog-messageList">{children}</div>
     </div>
